762b0aa2bd5edf93af3dd3bba2391a72eb7f556e
Manosij132 <ankur.majumder96@gmail.com>
2025-08-31 18:20:38 +0530

    Update provider.tf

--- Environement/provider.tf
+++ Environement/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
    backend "azurerm" {
-    resource_group_name  = "rgfortfstatemano"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "stgtfstatemano"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-   container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "rgfortfstatemano"          
+    storage_account_name = "stgtfstatemano"        
+   container_name       = "tfstate"                    
+    key                  = "prod.terraform.tfstate"             
   }
 }
 
